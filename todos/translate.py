6e06726474d75eac759ef08de9b527864360d1b1
toninoes <toninoes@gmail.com>
2021-05-09 14:41:44 +0000

    source auto

--- todos/translate.py
+++ todos/translate.py
@@ -17,7 +17,7 @@ def translate(event, context):
         }
     )
     
-    respuesta_trad = traductor.translate_text(Text=result['Item']['text'], SourceLanguageCode="es", TargetLanguageCode=event['pathParameters']['language'])
+    respuesta_trad = traductor.translate_text(Text=result['Item']['text'], SourceLanguageCode="auto", TargetLanguageCode=event['pathParameters']['language'])
     
     # create a response
     response = {
